6de7b1d0728b2e5e20063a520110717123abeece
etain <etainclub@gmail.com>
2019-09-27 00:50:31 +0900

    created app icon and splash screen

--- android/app/src/main/java/com/etainclub/MainActivity.java
+++ android/app/src/main/java/com/etainclub/MainActivity.java
@@ -1,5 +1,7 @@
 package com.etainclub;
 
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 
 import com.facebook.react.ReactActivityDelegate;
@@ -7,7 +9,11 @@ import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
