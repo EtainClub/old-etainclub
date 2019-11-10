c338c7ab3a5bd9fe2cbb4b5421574f08fa937760
etain <etain@etainclub.local>
2019-11-11 03:20:58 +0900

    applied codepush not tested.

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
  * @format
  */
 import {AppRegistry} from 'react-native';
+// code push
+import codePush from "react-native-code-push";
+
 import App from './app/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => codePush(App));
