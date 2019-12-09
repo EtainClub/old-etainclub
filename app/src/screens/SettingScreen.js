7d3d2ef433641d5fe5a53f5ceb9d6b7acda38b84
etain <etainclub@gmail.com>
2019-12-10 04:36:22 +0900

    created two clocks both for start and end time
    tried to creaate one list item but it had some re-rendering problem

--- app/src/screens/SettingScreen.js
+++ app/src/screens/SettingScreen.js
@@ -1,10 +1,8 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
 import { View, StyleSheet, Linking, Alert, Share } from 'react-native';
 import { NavigationEvents, SafeAreaView } from 'react-navigation';
 import { Text, SearchBar, ListItem, Divider } from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import MapView from 'react-native-maps';
 import i18next from 'i18next';
 import { useTranslation } from 'react-i18next';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -24,70 +22,75 @@ const SettingScreen = ({ navigation }) => {
     {
       title: t('SettingScreen.howto'),
       url: 'http://etain.club',
-      icon: 'thumb-up',
-      icon_url: 'https://cdn4.iconfinder.com/data/icons/common-toolbar/36/Help-2-48.png',
     },
     {
       title: t('SettingScreen.facebookGroup'),
       url: 'https://www.facebook.com/groups/497453057500529/',
-      icon: 'facebook-official',
-      icon_url: 'https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-48.png',
     },
     {
       title: t('SettingScreen.naverCafe'),
       url: 'https://cafe.naver.com/etainclub',
-      icon: 'naver',
-      icon_url: 'https://cdn3.iconfinder.com/data/icons/address-book-providers-in-black-white/512/naver-48.png',
     },
     {
       title: t('SettingScreen.github'),
       url: 'https://github.com/EtainClub/etainclub',
-      icon: 'github',
-      icon_url: 'https://cdn0.iconfinder.com/data/icons/typicons-2/24/social-github-circular-48.png',
     },
     {
       title: t('SettingScreen.terms'),
       url: 'https://etain.club/terms',
-      icon: 'github',
-      icon_url: 'https://cdn0.iconfinder.com/data/icons/typicons-2/24/social-github-circular-48.png',
     },
   ];
 
   const settingList = [
     {
       title: t('SettingScreen.donotdisturb'),
-      icon: 'notification'
     },
     {
       title: t('SettingScreen.language'),
-      icon: 'notification'
     },
     {
       title: t('SettingScreen.share'),
-      icon: 'home',
-      icon_url: 'https://cdn4.iconfinder.com/data/icons/43-social-media-line-icons/24/Share-48.png',
     },
     {
       title: t('SettingScreen.app'),
-      icon: 'howto'
     },
     {
       title: t('SettingScreen.evaluate'),
     },
     {
       title: t('SettingScreen.signout'),
-      icon: 'signout'
     },
     {
       title: t('SettingScreen.deleteAccount'),
-      icon: 'delete'
     },
   ];
+
+  const [, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
+
+  let count = 0;
 
   //// states
   // show do not disturb (DND) time list
   const [showDND, setShowDND] = useState(false); 
   const [showDNDClock, setShowDNDClock] = useState(false); 
+  const [showStartClock, setShowStartClock] = useState(false); 
+  const [showEndClock, setShowEndClock] = useState(false); 
+
+  // flag: is it setting the start time? 
+  const [startDND, setStartDND] = useState(true);
+  const [updateDNDTime, setUpdateDNDTime] = useState(false); 
+  const [startDNDTime, setStartDNDTime] = useState(null);
+  const [endDNDTime, setEndDNDTime] = useState(null);
+
+  // use effect
+  useEffect(() => {
+    // 
+//    console.log('[useEffect] showDNDClock', showDNDClock);
+    // re-render
+//    updateState();
+  }, [showDNDClock]);
+ 
   const onLinkPress = url => {
     Linking.openURL(url);  
   };
@@ -119,10 +122,10 @@ const SettingScreen = ({ navigation }) => {
           t('SettingScreen.evaluationTitle'),
           t('SettingScreen.evaluationText'),
           [
-            {text: t('no'), style: 'cancel' },
-            {text: t('yes')}
+            { text: t('no'), style: 'cancel' },
+            { text: t('yes') }
           ],
-          {cancelable: true},
+          { cancelable: true },
         );
         break;  
       case 5:
@@ -130,10 +133,10 @@ const SettingScreen = ({ navigation }) => {
           t('SettingScreen.signoutTitle'),
           t('SettingScreen.signoutText'),
           [
-            {text: t('no'), style: 'cancel' },
-            {text: t('yes'), onPress: () => signout({ navigation })}
+            { text: t('no'), style: 'cancel' },
+            { text: t('yes'), onPress: () => signout({ navigation }) }
           ],
-          {cancelable: true},
+          { cancelable: true },
         );
         break;  
       case 6:
@@ -141,32 +144,112 @@ const SettingScreen = ({ navigation }) => {
             t('SettingScreen.deleteTitle'),
             t('SettingScreen.deleteText'),
             [
-              {text: t('confirm')}
+              { text: t('confirm') }
             ],
-            {cancelable: true},
+            { cancelable: true },
           );
           break;  
       default:
     }
   };
 
+  // update swith state when a user clicks the DND time switch
   const onDNDValueChange = (value) => {
     console.log('[onDNDValueChange] value', value);
     // update the state
     setShowDND(value);
   };
 
-  const onDNDTimePress = () => {
+  // show clock when a user clicks the time list item
+  const onStartTimePress = () => {
+    setShowStartClock(true);
+    setShowEndClock(false);
+/*
     // show dialog
     Alert.alert(
       t('SettingScreen.dndTimeTitle'),
       t('SettingScreen.dndTimeText'),
       [
-        {text: t('confirm'), onPress: () => { setShowDNDClock(true) }}
+        // show clock
+        { text: t('confirm'), onPress: () => { console.log('onPress'); setShowDNDClock(true); } }
       ],
-      {cancelable: true},
+      { cancelable: true },
     );
+*/
   };
+
+  // show clock when a user clicks the time list item
+  const onEndTimePress = () => {
+    setShowStartClock(false);
+    setShowEndClock(true);
+  };
+
+  // when a user clicks ok or cancel button on clock
+  const onClockChange = (event) => {
+    console.log('[onClockChange] event', event);
+    console.log('[onClockChange] startDND', startDND);
+    // when a user cancels
+    if (event.type === 'dismissed') {
+      // make the clock invisible
+      setShowDNDClock(false);
+      // reset start flag
+      setStartDND(true);
+      console.log('[onClockChange] user canceled');
+      return;
+    }
+
+    setShowDNDClock(false);
+    return;
+    //// user clicks ok
+    // is it start time?
+    if (startDND) {
+      // save the start time
+//      setStartDNDTime(event.nativeEvent.timestamp);
+      // set startDND to false
+      setStartDND(false);
+      // ???
+      setShowDNDClock(false);
+      console.log('[onClockChange] start time selected');
+      return;
+    } 
+    // save the end time
+//    setEndDNDTime(event.nativeEvent.timestamp);
+    // set startDND to true
+    setStartDND(true);
+    // make the clock invisible
+    setShowDNDClock(false);
+    console.log('[onClockChange] end time selected. showDNDClock', showDNDClock);
+  };
+
+  // show clock
+  const renderStartClock = () => {
+    if (showStartClock) {
+      return (
+        <DateTimePicker 
+          display="spinner"
+          value={ new Date() }
+          mode={'time'}
+          is24Hour={false}
+          display="default"
+          onChange={(event) => {onClockChange(event)}} />
+      );
+    }
+  }
+
+  // show clock
+  const renderEndClock = () => {
+    if (showEndClock) {
+      return (
+        <DateTimePicker 
+          display="spinner"
+          value={ new Date() }
+          mode={'time'}
+          is24Hour={false}
+          display="default"
+          onChange={(event) => {onClockChange(event)}} />
+      );
+    }
+  }
 
   return (
     <SafeAreaView>
@@ -200,15 +283,25 @@ const SettingScreen = ({ navigation }) => {
                     value: showDND,
                     onValueChange: (value) => onDNDValueChange(value)
                   }}
-                  onPress={() => onSettingPress(i)}
                 />
                 {
-                    showDND &&
+                    showDND && 
+                    <View>
                     <ListItem
                       key={i+100}
-                      title={"11:00 PM" + " " + "-" + " " + "08:00 AM"}
-                      onPress={() => onDNDTimePress(i)}
+                      title={"Start: 11:00 PM"}
+                      containerStyle={{ backgroundColor: 'orange' }}
+                      chevron
+                      onPress={() => onStartTimePress(i)}
+                    />
+                    <ListItem
+                      key={i+101}
+                      title={"End: 08:00 AM"}
+                      containerStyle={{ backgroundColor: 'orange' }}
+                      chevron
+                      onPress={() => onEndTimePress(i)}
                     />        
+                    </View>
                 }
               </View>
               :
@@ -221,15 +314,8 @@ const SettingScreen = ({ navigation }) => {
           ))
         }
       </Spacer>
-        { showDNDClock && 
-          <DateTimePicker 
-            display="spinner"
-            value={ new Date() }
-            mode={'time'}
-            is24Hour={false}
-            display="default"
-            onChange={() => console.log('timepicker')} />
-        }  
+      {renderStartClock()}
+      {renderEndClock()}
       </ScrollView>
     </SafeAreaView>
   );
